48dbe6b33873b829f471611fdc1682afa0c4ed9f
Thomas <joke-the-mad@hotmail.com>
2019-07-11 15:17:38 +0200

    Création du formulaire genre

--- src/Form/BookFormType.php
+++ src/Form/BookFormType.php
@@ -2,8 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Author;
 use App\Entity\Book;
+use App\Entity\BookGenre;
+use App\Entity\Publisher;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +17,27 @@ class BookFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('price')
+            ->add('title', TextType::class, [
+                "label" => "Titre"
+            ])
+            ->add('price', TextType::class, [
+                "label" => "Prix"
+            ])
+            ->add('author', EntityType::class, [
+                "label" => "Auteur",
+                "class" => Author::class,
+                "choice_label" => "fullName"
+            ])
+            ->add('publisher', EntityType::class, [
+                "label" => "Editeur",
+                "class" => Publisher::class,
+                "choice_label" => "name"
+            ])
+            ->add('bookGenre', EntityType::class, [
+                "label" => "Genre",
+                "class" => BookGenre::class,
+                "choice_label" => "name"
+            ])
         ;
     }
 
